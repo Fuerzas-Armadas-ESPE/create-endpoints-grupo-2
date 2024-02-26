afb4cdb3762cd8390c128eddbdebea37525f2a99
mspadron <mspadron@espe.edu.ec>
2024-02-26 07:34:39 -0500

    Cambio de puerto a 27027:27017

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,7 +4,7 @@ import { PostsModule } from './posts/posts.module';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://root:example@localhost:27017'),
+    MongooseModule.forRoot('mongodb://root:example@localhost:27027/'),
     PostsModule,
   ],
 })
